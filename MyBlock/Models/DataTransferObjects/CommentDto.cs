192dda17bad2363b2cce192044721c91d707109c
DimitrovYordan <y.dimitrov84@abv.bg>
2023-01-08 15:41:47 +0200

    Create Dto classes.

---
+++ MyBlock/Models/DataTransferObjects/CommentDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MyBlock.Models.DataTransferObjects
+{
+    public class CommentDto
+    {
+        public int? AuthorID { get; set; }
+        public string Author { get; set; }
+        public string Content { get; set; }
+        public int ParentPostID { get; set; }
+        public int? ParentCommentID { get; set; }
+        public DateTime TimePosted { get; set; }
+    }
+}
---
+++ MyBlock/Models/DataTransferObjects/PostDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MyBlock.Models.DataTransferObjects
+{
+    public class PostDto
+    {
+        public int AuthorID { get; set; }
+        public string AuthorUsername { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int CommentsCount { get; set; }
+        public DateTime TimePosted { get; set; }
+    }
+}
